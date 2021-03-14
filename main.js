b80461e176c150989b965aa0cd85fd7981e64eb8
daisukey623 <daisuke.y623@gmail.com>
2021-03-14 18:47:35 +0900

    修正:主に条件内の修正。

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// 二回目以降、実行を押下したときには前回の結果が消去され、今回分の結果が表示されるように修正しましょう。
-
-// const reset =() => {
-//   console.log('resetします')
-// }
-
 const show = () => {
   // 初期化する
   const outputNum = document.getElementById('outputNum');
@@ -13,54 +7,48 @@ const show = () => {
   let inputFizz = document.getElementById('input-fizz').value ;
   let inputBuzz = document.getElementById('input-buzz').value ;
   
-  
-  
   // 入力値を数値型に型変換する
   let FizzNum = Number(inputFizz);
   let BuzzNum = Number(inputBuzz);
   
-  
   // 共通の出力
   const Output = `【出力】`;
   document.getElementById('output-message').innerHTML = Output;
-  
 
-  // 入力値毎に分岐して出力する
-  for(let i = 1; i < 100; i++) {
-    // 整数か判断する
-    if(Number.isInteger(FizzNum) === false || inputFizz === '' && inputBuzz === ''){ 
-      let val = '整数値を入力してください'
-      const li = document.createElement('li');
-      outputNum.appendChild(li);
-      li.textContent = val; 
-      break;
-
-      // 両方の倍数を表示する
-    } else if (i % FizzNum === 0 && i % BuzzNum === 0) {
-      let FizzBuzz = i;
-      let val = `FizzBuzz${FizzBuzz}`
-      const li = document.createElement('li');
-      outputNum.appendChild(li);
-      li.textContent = val;   
-
-      // fizzNumに入力された値の倍数を表示する
-    } else if (i % FizzNum === 0){
-      let Fizz = i;
-      let val = `Fizz${Fizz}`
-      const li = document.createElement('li');
-      outputNum.appendChild(li);
-      li.textContent = val;   
-
-       // BuzzNumに入力された値の倍数を表示する
-    } else if(i % BuzzNum === 0) {
-      let Buzz = i;
-      let val = `Buzz${Buzz}`
-      const li = document.createElement('li');
-      outputNum.appendChild(li);
-      li.textContent = val;   
-
-    } else {
+  // 整数値か判断する
+  if(Number.isInteger(FizzNum) === false || Number.isInteger(BuzzNum) === false || inputFizz === '' && inputBuzz === ''){ 
+    const caution = '整数値を入力してください'
+    const li = document.createElement('li');
+    outputNum.appendChild(li);
+    li.textContent = caution; 
+  } else {
+    // もし整数値だったのなら次の繰り返し処理を行う
+    for(let i = 1; i < 100; i++) {
+      // 倍数の定数化
+      const FizzNumMultiple = i % FizzNum === 0;
+      const BuzzNumMultiple = i % BuzzNum === 0;
       
-    };
-  };
-};
+      // 両方の倍数を表示する
+      if (FizzNumMultiple && BuzzNumMultiple) {
+        const FizzBuzz = `FizzBuzz${i}`
+        const li = document.createElement('li');
+        outputNum.appendChild(li);
+        li.textContent = FizzBuzz;   
+  
+        // fizzNumに入力された値の倍数を表示する
+      } else if (FizzNumMultiple){
+        const Fizz = `Fizz${i}`
+        const li = document.createElement('li');
+        outputNum.appendChild(li);
+        li.textContent = Fizz;   
+  
+         // BuzzNumに入力された値の倍数を表示する
+      } else if(BuzzNumMultiple) {
+        const Buzz = `Buzz${i}`
+        const li = document.createElement('li');
+        outputNum.appendChild(li);
+        li.textContent = Buzz;   
+      } 
+    }
+  }   
+}
